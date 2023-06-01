658dcb451ba065884bdc8ec5e9c553da800ceabc
Court Schuett <schuettc@amazon.com>
2023-05-31 21:50:35 -0500

    fix: permissions

--- src/kinesis.ts
+++ src/kinesis.ts
@@ -1,8 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { Database } from '@aws-cdk/aws-glue-alpha';
 import { RemovalPolicy, Stack } from 'aws-cdk-lib';
+
 import { CfnTable } from 'aws-cdk-lib/aws-glue';
-import { Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+import {
+  Role,
+  ServicePrincipal,
+  PolicyDocument,
+  PolicyStatement,
+} from 'aws-cdk-lib/aws-iam';
 import { CfnDeliveryStream } from 'aws-cdk-lib/aws-kinesisfirehose';
 import { LogGroup, LogStream, RetentionDays } from 'aws-cdk-lib/aws-logs';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
@@ -22,7 +28,24 @@ export class KinesisResources extends Construct {
 
     const firehoseRole = new Role(this, 'FirehoseRole', {
       assumedBy: new ServicePrincipal('firehose.amazonaws.com'),
+      inlinePolicies: {
+        ['gluePolicy']: new PolicyDocument({
+          statements: [
+            new PolicyStatement({
+              actions: ['glue:GetTableVersions'],
+              resources: [
+                props.cdrDatabaseName.catalogArn,
+                props.cdrDatabaseName.databaseArn,
+                `arn:aws:glue:${Stack.of(this).region}:${
+                  Stack.of(this).account
+                }:table/${props.cdrDatabaseName.databaseName}/*`,
+              ],
+            }),
+          ],
+        }),
+      },
     });
+
 
     props.processedCdrsBucket.grantWrite(firehoseRole);
 
@@ -36,6 +59,8 @@ export class KinesisResources extends Construct {
       logGroup: firehoseLogGroup,
       logStreamName: 'processCdrs',
     });
+
+    firehoseLogGroup.grantWrite(firehoseRole);
 
     this.kinesisStream = new CfnDeliveryStream(
       this,
